1bdc3110fc322cf7f6749ff59d59dc206bf6e645
Joe Maltba <joe.maltba@gmail.com>
2024-04-17 00:05:16 -0400

    Add Job Page Prog

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,39 @@ import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage'
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, {jobLoader} from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
+
+
+
+const App = () => {
+  // Add New Job
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob)
+    })
+    return;
+  }
+
+  //Delete Job
+  const deleteJob = async (id) => {
+
+  }
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
     <Route index element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
-    <Route path='/jobs/:id' element={<JobPage />} loader ={jobLoader} />
+    <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+    <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob}/>} loader ={jobLoader} />
     <Route path='*' element={<NotFoundPage />} />
-
   </Route>
   )
 );
-
-const App = () => {
   return (
     <RouterProvider router={router} />
   )
